cb3811fd63aee74edf8efb9346a18cee6ae610cc
gsanthakumar <gsanthakumar@gammagamestudio.com>
2026-06-11 19:00:48 +0530

    Fix register notifications

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Register() {
   const [name, setName] = useState("");
